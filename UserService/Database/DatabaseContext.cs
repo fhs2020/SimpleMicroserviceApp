6a0e117368514cec8b5190ded07f7dc2ae94e401
Flavio H Sousa <flavioh007@gmail.com>
2021-06-12 22:20:22 -0300

    Added my project

---
+++ UserService/Database/DatabaseContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserService.Database.Entities;
+
+namespace UserService.Database
+{
+    public class DatabaseContext: DbContext 
+    {
+        public DbSet <User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(LocalDb)\MSSQLLocalDB;Initial Catalog=UserMicroservice;
+                                        Integrated Security=SSPI;");
+        }
+    }
+}
